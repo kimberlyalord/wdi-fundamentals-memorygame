fd0f369424521d852ce13d92ff92f364a3fc9dd4
Kimberly Lord <kimberlyalord@Kimberlys-MacBook-Pro.local>
2020-04-18 00:45:28 -0600

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+let cardOne = "queen";
+let cardTwo = "queen";
+let cardThree = "king";
+let cardFour = "king";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardThree);
+console.log("User flipped " + cardFour);
