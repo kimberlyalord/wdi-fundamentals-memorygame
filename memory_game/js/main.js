4689cbf6a51deef8a97aafb75358b5d83e65b192
Kimberly Lord <kimberlyalord@Kimberlys-MacBook-Pro.local>
2020-04-19 15:32:59 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,26 +1,26 @@
 const cards = ['queen', 'queen', 'king', 'king'];
 const cardsInPlay = [];
 
-let cardOne = cards[0];
-cardsInPlay.push('cardOne');
-
-let cardTwo = cards[2];
-cardsInPlay.push('cardTwo');
-
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
+function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-	}
-	else if (cardsInPlay[0] !== cardsInPlay[1]) {
-		alert("Sorry, try again.");
-	}
-else {
-	console.log('Flip another card!');
+		console.log("You found a match!");
+		}
+		else {
+		console.log("Sorry, try again.");
 	}
 }
 
-cardsInPlay;
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId])
+	cardsInPlay.push(cards[cardId]);
+
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+		}
+	else {
+		console.log('Flip another card!');
+		}
+	}	
+
+flipCard(0);
+flipCard(2);
